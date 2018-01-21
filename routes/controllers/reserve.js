69e9013ca560327693a5e6e3cad69917a09e43c2
fabs <faebeee@gmail.com>
2018-01-21 14:44:21 +0100

    update API

--- routes/controllers/reserve.js
+++ routes/controllers/reserve.js
@@ -1,14 +1,56 @@
 const keystone = require('keystone');
+const Twig = require('twig');
+const sendmail = require('sendmail')();
+
+const Steps = keystone.list('Step');
+
+
+function sendMailNotification(sponsor, step) {
+    console.log(sponsor);
+        Twig.renderFile(__dirname + '../../templates/email/confirmation.twig', {sponsor, step}, (err, html) => {
+            if (err) {
+                throw err;
+            }
+
+            sendmail({
+                from: 'no-reply@vorstadtsounds.ch',
+                //to: 'sponsorMail, kontakt@163.com ',
+                to: `faebeee@gmail.com`,
+                subject: 'Treppen Sponsor',
+                html: html,
+            }, function (err, reply) {
+                console.log(err && err.stack);
+                console.dir(reply);
+            });
+        });
+}
+
 
 exports = module.exports = function (req, res) {
+    const {firstname, lastname, email} = req.body;
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+    Steps.model.findOne({_id: req.params.id, /*isSold: false, isReserved: false*/}).exec(function (err, item) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+        if (!item) {
+            return res.status(404).send('Not found');
+        }
 
-	// Render the view
-	view.render('index');
+        item.update({
+            sponsor: {first: firstname, last: lastname},
+            sponsor_email: email,
+            isReserved: true
+        }, function (err, raw) {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+
+            sendMailNotification({firstname, lastname, email}, item);
+
+            return res.status(201).send(raw);
+
+        });
+    });
 };
