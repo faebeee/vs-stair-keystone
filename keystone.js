c6a6c6291fea528c501c4c358b49c5c14ba5c656
fabs <faebeee@gmail.com>
2018-01-23 19:42:53 +0100

    update CORS settings

--- keystone.js
+++ keystone.js
@@ -51,6 +51,8 @@ keystone.set('locals', {
 // Load your project's Routes
 keystone.set('routes', require('./routes'));
 keystone.set('cors allow origin', true);
+keystone.set('cors allow methods', true);
+keystone.set('cors allow headers', true);
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
