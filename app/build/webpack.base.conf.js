bb76cd82264b78d82abd97a0b563850d963f5970
fabs <faebeee@gmail.com>
2018-02-19 18:29:35 +0100

    New build

--- app/build/webpack.base.conf.js
+++ app/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path');
 const utils = require('./utils');
 const config = require('../config');
 const vueLoaderConfig = require('./vue-loader.conf');
+var ImageminPlugin = require("imagemin-webpack-plugin").default;
 
 
 function resolve(dir) {
@@ -21,57 +22,64 @@ const createLintingRule = () => ({
 })
 
 module.exports = {
-    context: path.resolve(__dirname, '../'),
-    entry: [
-        './src/main.js',
-        './src/scss/main.scss',
-    ],
+    context: path.resolve(__dirname, "../"),
+    entry: ["./src/main.js", "./src/scss/main.scss"],
     output: {
         path: config.build.assetsRoot,
-        filename: '[name].js',
-        publicPath: process.env.NODE_ENV === 'production'
-            ? config.build.assetsPublicPath
-            : config.dev.assetsPublicPath
+        filename: "[name].js",
+        publicPath:
+            process.env.NODE_ENV === "production"
+                ? config.build.assetsPublicPath
+                : config.dev.assetsPublicPath
     },
     resolve: {
-        extensions: ['.js', '.vue', '.json', '.scss'],
+        extensions: [".js", ".vue", ".json", ".scss"],
         alias: {
-            'vue$': 'vue/dist/vue.esm.js',
-            '@': resolve('src'),
-        },
+            vue$: "vue/dist/vue.esm.js",
+            "@": resolve("src")
+        }
     },
+    plugins: [
+        new ImageminPlugin({
+            pngquant: {
+                quality: "80"
+            }
+        })
+    ],
     module: {
-
         rules: [
-             {
+            {
                 test: /\.(woff|woff2|eot|ttf)$/,
-                loader: 'file',
-
+                loader: "file"
             },
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
+                loader: "vue-loader",
                 options: vueLoaderConfig
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
-                include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+                loader: "babel-loader",
+                include: [
+                    resolve("src"),
+                    resolve("test"),
+                    resolve("node_modules/webpack-dev-server/client")
+                ]
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    name: utils.assetsPath("img/[name].[hash:7].[ext]")
                 }
             },
             {
                 test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                    name: utils.assetsPath("media/[name].[hash:7].[ext]")
                 }
             }
         ]
@@ -82,10 +90,10 @@ module.exports = {
         setImmediate: false,
         // prevent webpack from injecting mocks to Node native modules
         // that does not make sense for the client
-        dgram: 'empty',
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty'
+        dgram: "empty",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty"
     }
-}
+};
