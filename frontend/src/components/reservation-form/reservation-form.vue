f7d472823df23ceaa01ecb6dca3b04a73288da26
fabs <faebeee@gmail.com>
2018-02-21 21:34:56 +0100

    Update build

--- frontend/src/components/reservation-form/reservation-form.vue
+++ frontend/src/components/reservation-form/reservation-form.vue
@@ -143,7 +143,6 @@
                         }
                     })
                     .finally(() => {
-                        this.load();
                         this.isLoading = false;
                     })
             }
